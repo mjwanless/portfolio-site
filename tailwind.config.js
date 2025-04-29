3a3522b769b2807d7743284df3a814a4c3079551
Malcolm Wanless <mwanless2@my.bcit.ca>
2025-04-28 22:43:53 -0700

    Code: custom colors set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "custom-darkgrey-1": "var(--custom-darkgrey-1)",
+                "custom-darkgrey-2": "var(--custom-darkgrey-2)",
+                "custom-cyan": "var(--custom-cyan)",
+                "custom-white": "var(--custom-white)",
+                "custom-pink": "var(--custom-pink)",
+                "custom-blue": "var(--custom-blue)",
+                "custom-orange": "var(--custom-orange)",
+                "custom-purple": "var(--custom-purple)",
+                "custom-darkpurple": "var(--custom-darkpurple)",
+                "custom-grey": "var(--custom-grey)",
+            },
+        },
     },
     plugins: [],
 };
