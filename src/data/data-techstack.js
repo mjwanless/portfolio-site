86dd54f6b14c595ac982404ef3296f0aebc8195b
Malcolm Wanless <mwanless2@my.bcit.ca>
2025-05-03 02:44:47 -0700

    Separate data into separate files

---
+++ src/data/data-techstack.js
@@ -0,0 +1,179 @@
+export const techStack = [
+    // Python Section
+    {
+        id: 1,
+        name: "Python",
+        category: "Languages",
+        description: "A versatile programming language used for web development, data analysis, automation, and game development.",
+        relatedTech: [
+            {
+                id: 101,
+                name: "Pandas",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pandas/pandas-original.svg",
+            },
+            {
+                id: 102,
+                name: "PyCharm",
+                icon: "https://upload.wikimedia.org/wikipedia/commons/a/a1/PyCharm_Icon.svg",
+            },
+            {
+                id: 103,
+                name: "Pygame",
+                icon: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Pygame_logo.svg",
+            },
+        ],
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    },
+
+    // JavaScript Section
+    {
+        id: 2,
+        name: "JavaScript",
+        category: "Languages",
+        description: "A dynamic programming language commonly used to create interactive effects within web browsers.",
+        relatedTech: [
+            {
+                id: 201,
+                name: "Node.js",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+            },
+            {
+                id: 202,
+                name: "React",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+            },
+            {
+                id: 203,
+                name: "Express",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+            },
+            {
+                id: 204,
+                name: "Three.js",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/threejs/threejs-original-wordmark.svg",
+            },
+            {
+                id: 205,
+                name: "GSAP",
+                icon: "https://upload.wikimedia.org/wikipedia/commons/3/30/GSAP_Logo.svg",
+            },
+            {
+                id: 206,
+                name: "Mongoose",
+                icon: "https://cdn.worldvectorlogo.com/logos/mongoose.svg",
+            },
+            {
+                id: 207,
+                name: "Vanta.js",
+                icon: "https://upload.wikimedia.org/wikipedia/commons/0/09/Vanta_logo.svg",
+            },
+        ],
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    },
+
+    // HTML/CSS Section
+    {
+        id: 3,
+        name: "HTML & CSS",
+        category: "Languages",
+        description: "The foundational building blocks of web development, used to structure and style web pages.",
+        relatedTech: [
+            {
+                id: 301,
+                name: "Bootstrap",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+            },
+            {
+                id: 302,
+                name: "Tailwind CSS",
+                icon: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+            },
+            {
+                id: 303,
+                name: "SASS",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+            },
+        ],
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    },
+
+    // Java Section
+    {
+        id: 4,
+        name: "Java",
+        category: "Languages",
+        description: "A high-level, class-based, object-oriented programming language that is widely used for building enterprise applications.",
+        relatedTech: [
+            {
+                id: 401,
+                name: "Kotlin",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/kotlin/kotlin-original.svg",
+            },
+            {
+                id: 402,
+                name: "LibGDX",
+                icon: "https://libgdx.badlogicgames.com/libgdx-logo.svg",
+            },
+        ],
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    },
+
+    // C and C++ Section
+    {
+        id: 5,
+        name: "C & C++",
+        category: "Languages",
+        description: "Low-level programming languages used for system programming, game development, and high-performance applications.",
+        relatedTech: [],
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    },
+    {
+        id: 6,
+        name: "C++",
+        category: "Languages",
+        description: "An extension of C, used for building complex applications with object-oriented features.",
+        relatedTech: [],
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
+    },
+
+    // Other Section
+    {
+        id: 7,
+        name: "Other",
+        category: "Tools & Platforms",
+        description: "Miscellaneous tools and platforms used in software development.",
+        relatedTech: [
+            {
+                id: 701,
+                name: "MATLAB",
+                icon: "https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png",
+            },
+            {
+                id: 702,
+                name: "Unity",
+                icon: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Unity_Logo.svg",
+            },
+            {
+                id: 703,
+                name: "R",
+                icon: "https://www.r-project.org/logo/Rlogo.svg",
+            },
+            {
+                id: 704,
+                name: "Linux",
+                icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg",
+            },
+            {
+                id: 705,
+                name: "Figma",
+                icon: "https://www.vectorlogo.zone/logos/figma/figma-icon.svg",
+            },
+            {
+                id: 706,
+                name: "GitHub",
+                icon: "https://cdn.worldvectorlogo.com/logos/github-icon.svg",
+            },
+        ],
+        icon: "https://upload.wikimedia.org/wikipedia/commons/5/55/Generic_development_tools.svg",
+    },
+];
